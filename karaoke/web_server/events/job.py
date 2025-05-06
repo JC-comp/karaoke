f2eab0f456347ca58669a90bb21b00ebc5495bcf
JComp <alan@jcomp.tk>
2025-05-06 12:30:57 +0000

    Add job monitor joined callback

--- karaoke/web_server/events/job.py
+++ karaoke/web_server/events/job.py
@@ -18,11 +18,13 @@ class JobBinderManager(BinderManager):
 
     def _send_latest_progress(self, scheduler: SchedulerBinder, sid: str) -> None:
         scheduler.send_progress_to_sid(self.socketio, sid, self.namespace)
+        self.socketio.emit('joined', 'ok', room=sid, namespace=self.namespace)
    
     def _serving_progress_update(self, scheduler: SchedulerBinder, job_id: str) -> None:
         """
         Serve the progress update to the client via socketio.
         """
+        self.socketio.emit('joined', 'ok', room=job_id, namespace=self.namespace)
         scheduler.listen_progress_by_jobId(self.socketio, job_id, self.namespace)
         # The only case where the scheduler is closed is when scheduler is gone first or all
         # clients are gone, clients are notified within the scheduler if an error occurs, 
